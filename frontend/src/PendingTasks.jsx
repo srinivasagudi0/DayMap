9fef0a46b6ee6d365d9fd0fa2f365c66f7db9a33
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-08-30 11:08:22 -0500

    the app now succesfully shows today tasks

--- frontend/src/PendingTasks.jsx
+++ frontend/src/PendingTasks.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 
 function Pending() {
+
+    const [dueToday, setDueToday] = useState([]);
+
+useEffect(() => {
+fetch('/todays-tasks')
+    .then(response => response.json())
+    .then(data => setDueToday(data.due))
+    .catch(error => console.error('Error fetching tasks', error));
+}, [])
     return (
         <main>
-        <div class="title">
-        <h1>Pending Tasks</h1>
-        <p>See your tasks here!</p>
+        <div className="title">
+            <h1>Pending Tasks</h1>
+            <p>See your tasks here!</p>
         </div>
         <br />
+        <div className="today-tasks">
+        <h2>Today's Tasks</h2>
+        <ul style={{"borderRadius": "50px", "border": "2px solid #2c3e50"}}>
+            {dueToday.map(task => (
+            <li key={task[0]}>
+                {task[1]} - {task[2]}
+            </li>
+            ))}
+        </ul>
+        </div>
         </main>
     )
 }
 
 export default Pending;
-
+ 
