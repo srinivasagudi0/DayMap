e136fe55fe9d33eeea47147de23aec3b2bd67ef6
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-12 08:00:04 -0500

    bugs are fixed

--- frontend/src/FutureRoom.jsx
+++ frontend/src/FutureRoom.jsx
@@ -163,7 +163,7 @@ function FutureRoom() {
         const minutes = Math.floor(seconds/60);
         const remainingSeconds = seconds % 60;
 
-        return `${minutes}:${remainingSeconds.toString().padStart(2,0)}`;
+        return `${minutes}:${remainingSeconds.toString().padStart(2,"0")}`;
     }
 
 
