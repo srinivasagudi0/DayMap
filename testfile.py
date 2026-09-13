26b77742097a039106224afb329a298d41968389
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-12 21:05:42 -0500

    wrote the readme by myself

--- testfile.py
+++ testfile.py
@@ -6,6 +6,8 @@ def delete():
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
     cursor.execute('DELETE FROM future_task_room')
+    cursor.execute('DELETE FROM tasks')
+    cursor.execute('DELETE FROM completed_tasks')
     conn.commit()
     conn.close()
     return "done"
